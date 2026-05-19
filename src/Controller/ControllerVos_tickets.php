b98d60dcf332a2c4d0bb0616dd8882da96595c4e
Timéo Dupé <titi.dudu.tv@gmail.com>
2026-05-19 10:53:30 +0200

    Ajout du header et du footer sur la page Vos tickets

--- src/Controller/ControllerVos_tickets.php
+++ src/Controller/ControllerVos_tickets.php
@@ -1,6 +1,6 @@
 <?php
 // ControllerVos_tickets.php
 // Fichier qui permet de gérer la page Vos tickets
-require_once __DIR__ . '/..Model/ModelVos_tickets.php';
+require_once __DIR__ . '/../Model/ModelVos_tickets.php';
 // Traitement des données pour l'affiche sur la page Vos tickets
-require_once __DIR__ . '/../View/Vos_tickets.php';
+require_once __DIR__ . '/../View/vos_tickets.php';
