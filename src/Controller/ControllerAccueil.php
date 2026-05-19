e49b87d3c1371bc472b64c23312f735640ca4bbe
Timéo Dupé <titi.dudu.tv@gmail.com>
2026-05-19 10:34:00 +0200

    Ajout du Controller et du Model pour la page Vos_tickets

--- src/Controller/ControllerAccueil.php
+++ src/Controller/ControllerAccueil.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 // ControllerAccueil.php
 // Fichier qui permet de gérer la page d'accueil
 require_once __DIR__ . '/../Model/ModelAccueil.php';
-//Traitement de tes données pour l'affichage sur la page d'accueil
+//Traitement des onnées pour l'affichage sur la page d'accueil
 require_once __DIR__ . '/../View/Accueil.php';
---
+++ src/Model/ModelVos_tickets.php
@@ -0,0 +1,3 @@
+<?php 
+// ModelAccueil.php
+// Fichier qui permet de récuperer les données pour le controlleur de la page Vos tickets
