3df92c97f4fd38a666f68ad3aa905b09cf790c62
Timéo Dupé <titi.dudu.tv@gmail.com>
2026-05-19 14:44:41 +0200

    Ajout de la page Premier ticket

---
+++ src/Controller/ControllerPremier_ticket.php
@@ -0,0 +1,6 @@
+<?php
+// ControllerPremier_ticket.php
+// Fichier qui permet de gérer la page Premier ticket
+require_once __DIR__ . '/../Model/ModelPremier_ticket.php';
+// Traitement des données pour l'affichage sur la page Premier ticket
+require_once __DIR__ . '/../View/Premier_ticket.php';
---
+++ src/Model/ModelPremier_ticket.php
@@ -0,0 +1,3 @@
+<?php 
+// ModelPremierTicket.php
+// Fichier qui permet de récuperer les données pour le controlleur de la page Premier ticket
