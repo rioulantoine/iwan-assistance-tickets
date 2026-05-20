98b23476e84d33c4308037aa0da88f4546861fb6
Timéo Dupé <titi.dudu.tv@gmail.com>
2026-05-20 09:28:06 +0200

    Ajout du bouton pour télécharger les pieces jointes dans la page Détails ticket

--- src/View/Details_ticket.php
+++ src/View/Details_ticket.php
@@ -47,6 +47,34 @@
                 <div class="message-body">
                     <h3>Le site est inaccessible</h3>
                     <p class="message-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <div class="message-attachments">
+
+                        <a href="#" class="attachment-card" download>
+
+                            <div class="attachment-icon">
+                                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                                    <polyline points="14 2 14 8 20 8"></polyline>
+                                    <text x="6" y="16" font-size="6" font-weight="bold" fill="#e15252" stroke="none">PDF</text>
+                                </svg>
+                            </div>
+
+                            <div class="attachment-info">
+                                <span class="attachment-name">File Title.pdf</span>
+                                <span class="attachment-size">313 KB</span>
+                            </div>
+
+                            <div class="attachment-download-icon">
+                                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                                    <polyline points="7 10 12 15 17 10"></polyline>
+                                    <line x1="12" y1="15" x2="12" y2="3"></line>
+                                </svg>
+                            </div>
+
+                        </a>
+
+                    </div>
                 </div>
             </div>
 
