d471415d2f7179e83cf08759e5f71016d0378abf
Timéo Dupé <titi.dudu.tv@gmail.com>
2026-05-21 18:02:42 +0200

    Bouton de la page les Tickets dans le header

--- src/View/Templates/Header.php
+++ src/View/Templates/Header.php
@@ -21,6 +21,24 @@
             </svg>
             <span>Tableau de bord</span>
         </a>
+        <?php if (in_array((int)($role_utilisateur ?? 0), [1, 2])): ?>
+            <a href="index.php?page=admin_tickets" class="link-with-icon">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round">
+                    <path
+                        d="M4 20h16a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.93a2 2 0 0 1-1.66-.9l-.82-1.2A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2z"></path>
+                </svg>
+                <span>Les tickets</span>
+            </a>
+        <?php endif; ?>
 
         <a href="index.php?page=tickets" class="link-with-icon">
             <svg
