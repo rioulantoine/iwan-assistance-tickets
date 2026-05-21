ce8d69e6578020be848c4db5b1c6ed40258179eb
Timéo Dupé <titi.dudu.tv@gmail.com>
2026-05-21 10:31:35 +0200

    Fichier d'authentification pour les tests

---
+++ auth.php
@@ -0,0 +1,20 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+require_once 'src/Model/ModelBDD.php';
+
+$id_user_test = 1;
+$pdo = get_bdd();
+$stmt = $pdo->prepare("SELECT * FROM USER WHERE id_user = ?");
+$stmt->execute([$id_user_test]);
+$user = $stmt->fetch();
+
+if ($user) {
+    $_SESSION['user_id']   = $user['id_user'];
+    $_SESSION['user_nom']  = $user['nom'];
+    $_SESSION['user_role'] = $user['id_role'];
+} else {
+    die("Erreur : L'ID de test n'existe pas dans ton jeu d'essai !");
+}
