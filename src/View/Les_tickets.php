8ada66f4690cca8266566694e682c2ed5142d3ac
Timéo Dupé <titi.dudu.tv@gmail.com>
2026-05-21 18:02:12 +0200

    Ajout de la page View Les_tickets

---
+++ src/View/Les_tickets.php
@@ -0,0 +1,14 @@
+<?php require_once __DIR__ . '/Templates/Header.php'; ?>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Tableau de bord</title>
+    <link rel="icon" type="image/png" href="img/Logo_Iwan.png" />
+    <link rel="stylesheet" href="public/styles/Les_ticket.css" />
+    <link rel="stylesheet" href="public/styles/Global.css" />
+</head>
+<main>
+
+</main>
+<?php require_once __DIR__ . '/Templates/Footer.php'; ?>
