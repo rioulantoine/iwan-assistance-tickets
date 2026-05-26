e266a38d80656282eb6c889c840df29453b18ab0
Timéo Dupé <titi.dudu.tv@gmail.com>
2026-05-26 10:05:03 +0200

    supression table user et role et modification de la table tickets et reponse

--- IWAN_TICKETS.sql
+++ IWAN_TICKETS.sql
@@ -1,63 +1,47 @@
-CREATE TABLE ROLES(
-   id_role INT AUTO_INCREMENT,
-   libelle_role VARCHAR(30) NOT NULL,
-   PRIMARY KEY(id_role),
-   UNIQUE(libelle_role)
-);
-
-CREATE TABLE USER(
-   id_user INT AUTO_INCREMENT,
-   nom VARCHAR(100) NOT NULL,
-   mot_de_passe VARCHAR(255) NOT NULL,
-   date_creation DATETIME NOT NULL,
-   id_role INT NOT NULL,
-   PRIMARY KEY(id_user),
-   FOREIGN KEY(id_role) REFERENCES ROLES(id_role)
-);
-
 CREATE TABLE STATUT(
    id_statut INT AUTO_INCREMENT,
    libelle_statut VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id_statut),
-   UNIQUE(libelle_statut)
+   PRIMARY KEY(id_statut)
 );
 
 CREATE TABLE NIVEAU_URGENCE(
    id_urgence INT AUTO_INCREMENT,
    libelle_urgence VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id_urgence),
-   UNIQUE(libelle_urgence)
+   PRIMARY KEY(id_urgence)
 );
 
 CREATE TABLE TICKETS(
    id_ticket INT AUTO_INCREMENT,
    numero_ticket VARCHAR(30) NOT NULL,
-   declarant_nom VARCHAR(50),
+   declarant_nom VARCHAR(50) NOT NULL,
    declarant_prenom VARCHAR(50),
-   declarant_email VARCHAR(100),
+   declarant_telephone VARCHAR(16),
+   declarant_email VARCHAR(100) NOT NULL,
    titre VARCHAR(100) NOT NULL,
    description TEXT NOT NULL,
    date_creation DATETIME NOT NULL,
    date_archivage DATETIME NULL,
+   date_resolution DATETIME NULL,
+   id_entreprise VARCHAR(50) NOT NULL,
+   nom_entreprise VARCHAR(100) NOT NULL,
    id_urgence INT NOT NULL,
-   id_user INT NOT NULL,
    id_statut INT NOT NULL,
    PRIMARY KEY(id_ticket),
    UNIQUE(numero_ticket),
    FOREIGN KEY(id_urgence) REFERENCES NIVEAU_URGENCE(id_urgence),
-   FOREIGN KEY(id_user) REFERENCES USER(id_user),
    FOREIGN KEY(id_statut) REFERENCES STATUT(id_statut)
 );
 
 CREATE TABLE REPONSE(
    id_reponse INT AUTO_INCREMENT,
-   titre VARCHAR(100),
+   titre VARCHAR(100) NOT NULL,
    contenu TEXT NOT NULL,
    date_envoi DATETIME NOT NULL,
-   id_user INT NOT NULL,
+   auteur_nom VARCHAR(50) NOT NULL,
+   auteur_prenom VARCHAR(50) NOT NULL,
+   auteur_type VARCHAR(50) NOT NULL,
    id_ticket INT NOT NULL,
    PRIMARY KEY(id_reponse),
-   FOREIGN KEY(id_user) REFERENCES USER(id_user),
    FOREIGN KEY(id_ticket) REFERENCES TICKETS(id_ticket) ON DELETE CASCADE
 );
 
