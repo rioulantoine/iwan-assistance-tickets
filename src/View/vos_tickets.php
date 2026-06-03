08a24d9e840ca97d6275b60ad9d8e4e5b5d87951
Timéo Dupé <titi.dudu.tv@gmail.com>
2026-06-03 14:43:28 +0200

    Modification header

--- src/View/vos_tickets.php
+++ src/View/vos_tickets.php
@@ -12,31 +12,30 @@
 
 <body>
     <main>
-        <div class="container-tickets">
-            <h1>Historique de mes tickets</h1>
-            <p>
-                Retrouvez ici l'historique et l'état d'avancement de toutes vos
-                sollicitations.
-            </p>
+        <div class="tableau-de-bord">
+            <div class="header-text">
+                <h1>Tableau de bord</h1>
+                <p>
+                    Retrouvez ici l'historique et l'état d'avancement de toutes vos
+                    sollicitations.
+                </p>
+            </div>
 
-            <a href="index.php?page=nouveau_ticket" class="btn-nouveau-ticket">
-                <div class="icon-button">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <path d="M12 20h9"></path>
-                        <path d="M16.5 3.5a2.12 2.12 0 0 1 3 3L7 19l-4 1 1-4Z"></path>
-                    </svg>
-                </div>
-                <span>Nouveau ticket</span>
-            </a>
+            <?php if (isset($_SESSION['id_client'])): ?>
+                <a href="index.php?page=nouveau_ticket" class="btn-nouveau-ticket">
+                    <div class="icon-button">
+                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                        </svg>
+                    </div>
+                    <span>Nouveau ticket</span>
+                </a>
+            <?php endif; ?>
+        </div>
+        <div class="container-tickets">
+
+
             <div class="dashboard-content">
                 <!-- Filtre des tickets -->
                 <div class="filtre-ticket">
