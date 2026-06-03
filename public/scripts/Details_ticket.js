65c7ddd0a5ae1be71aa7239f614776382e93acac
Timéo Dupé <titi.dudu.tv@gmail.com>
2026-06-03 15:39:32 +0200

    commentaires

--- public/scripts/Details_ticket.js
+++ public/scripts/Details_ticket.js
@@ -129,14 +129,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const actionsBox = document.querySelector('.textarea-actions');
     
     if (textarea && actionsBox) {
-        // 1. On mesure la hauteur réelle de la zone contenant les fichiers et le bouton
         const actionsHeight = actionsBox.offsetHeight;
         
-        // 2. On pousse le "fond" (padding) du textarea vers le bas pour ne pas cacher le texte
-        // (actionsHeight + 15 pixels de marge de sécurité)
         textarea.style.paddingBottom = (actionsHeight + 15) + 'px';
         
-        // 3. On "simule" une frappe au clavier pour forcer l'auto-resize qu'on a codé tout à l'heure !
         textarea.dispatchEvent(new Event('input'));
     }
         }
