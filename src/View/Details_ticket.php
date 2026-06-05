a0460b32ae2184e0759b0f18b27640942a65790a
Timéo Dupé <titi.dudu.tv@gmail.com>
2026-06-05 11:00:26 +0200

    Affichage et gestion d'erreur en cas de nuémro de tel avec long text

--- src/View/Details_ticket.php
+++ src/View/Details_ticket.php
@@ -216,7 +216,7 @@
                             </svg><strong>Email :</strong> <?= htmlspecialchars($details_ticket['declarant_email'] ?? 'Email non spécifié') ?></p>
                         <p><svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
-                            </svg><strong>Téléphone :</strong> <?= htmlspecialchars($details_ticket['declarant_telephone'] ?? 'Numéro non spécifié') ?></p>
+                            </svg><strong>Téléphone :</strong> <span class="info-tel"><?= htmlspecialchars($details_ticket['declarant_telephone'] ?? 'Numéro non spécifié') ?></span></p>
                     </div>
                     <p class="message-text"><?= htmlspecialchars($details_ticket['description'] ?? 'Description non spécifiée') ?></p>
 
