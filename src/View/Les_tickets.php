5891221727b81eecc5c158ebfd294ebef8c41607
Timéo Dupé <titi.dudu.tv@gmail.com>
2026-06-10 09:29:47 +0200

    Ajout du bouton nouveau ticket

--- src/View/Les_tickets.php
+++ src/View/Les_tickets.php
@@ -11,7 +11,15 @@
 </head>
 <main>
     <div class="container-tickets">
-
+        <a href="index.php?page=nouveau_ticket" class="btn-nouveau-ticket">
+            <div class="icon-button">
+                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                </svg>
+            </div>
+            <span>Nouveau ticket</span>
+        </a>
         <div class="dashboard-content">
             <h1>Les tickets</h1>
 
